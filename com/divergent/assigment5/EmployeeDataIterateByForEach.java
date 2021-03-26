fb222ae44a734a060a944a13c7e69c44c9d3d94b
Prateek Patel <prateek.patel@divergentsl.com>
2021-03-26 19:22:32 +0530

    Today task And Theory Question Commit

---
+++ com/divergent/assigment5/EmployeeDataIterateByForEach.java
@@ -0,0 +1,43 @@
+package com.divergent.assigment5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Employee1 Class
+ * Employee1 Class Parameterized Custructor
+ * @author Divergent
+ *
+ */
+class Employee1 {
+	String name, dept;
+	int id;
+
+	Employee1(int id, String name, String dept) {
+		this.id = id;
+		this.name = name;
+		this.dept = dept;
+	}
+}
+
+/**
+ * Employee Data Iterate By For Each Loop
+ * 
+ * @author Divergent
+ *
+ */
+public class EmployeeDataIterateByForEach {
+
+	public static void main(String[] args) {
+		List<Employee1> list = new ArrayList<>();
+		list.add(new Employee1(101, "Ravi", "Account"));
+		list.add(new Employee1(102, "Ravindra", "Staff"));
+		list.add(new Employee1(103, "Shyam", "Teacher"));
+
+		for (Employee1 emp : list) {
+			System.out.println(emp.id + " " + emp.name + " " + emp.dept);
+		}
+
+	}
+
+}
---
+++ com/divergent/assigment5/StaticFactoryMethodUsedClass.java
@@ -0,0 +1,71 @@
+package com.divergent.assigment5;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * Employee Class
+ * 
+ * @author Divergent
+ *
+ */
+class Employee {
+	String name;
+	Double salary;
+	Date hireDate;
+
+	/**
+	 * Private Employee Custructor
+	 * 
+	 * @param name
+	 * @param salary
+	 * @param hireDate
+	 */
+	private Employee(String name, Double salary, Date hireDate) {
+		Objects.requireNonNull(hireDate, "NullPointerException");
+
+		this.name = name;
+		this.salary = salary;
+		this.hireDate = hireDate;
+	}
+
+	public static Employee getInstance(String name, Double salary, Date hireDate) {
+		return new Employee(name, salary, hireDate);
+	}
+
+	public String toString() {
+		return name + " " + salary + " " + hireDate;
+	}
+
+	public boolean equals(Employee employee) {
+		return employee.name.equals(name) && employee.salary.equals(salary);
+	}
+}
+
+/**
+ * Static Factory Methods
+ * 
+ * @author Divergent
+ *
+ */
+public class StaticFactoryMethodUsedClass {
+
+	public static void main(String[] args) {
+		Employee[] emp = new Employee[3];
+		emp[0] = Employee.getInstance("Shamu", 52000.0, new Date());
+		emp[1] = Employee.getInstance("Ravina", 12200.0, new Date());
+		emp[2] = Employee.getInstance("Ramu", 82000.0, new Date());
+
+		for (Employee emp1 : emp) {
+			System.out.println(emp1);
+		}
+
+		/**
+		 * Check Equals GetInstance Method
+		 */
+		Employee em = Employee.getInstance("Ramu", 82000.0, new Date());
+		System.out.println(em.equals(emp[2]));
+
+	}
+
+}
